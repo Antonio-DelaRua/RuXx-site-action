0bcab23caf22951cc1ae73a1172f63629c4d8087
Antonio-DelaRua <ruxxdeveloper@gmail.com>
2025-10-09 17:30:19 +0200

    page-skills

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -10,45 +10,49 @@ export class ProjectService {
         title: 'Angular Firebase',
         description: 'Una aplicación web basada en una wikipedia de coches, realizada con Angular y Firebase.',
         technologies: ['Angular', 'Firebase', 'TypeScript', 'SCSS', 'RxJS'],
-        demoUrl: 'https://github.com',
-        githubUrl: 'https://github.com',
-        image: 'assets/ang1.jpg'
+        demoUrl: 'https://github.com/Antonio-DelaRua/login-firebase',
+        githubUrl: 'https://github.com/Antonio-DelaRua',
+        image: 'assets/angular.gif'
       },
       {
         title: 'React E-commerce',
         description: 'Sistema de gestión de tareas con funcionalidades CRUD completas.',
         technologies: ['React', 'Node.js', 'Javascript', 'RxJS', 'PostgresSQL'],
-        demoUrl: 'https://demo-taskmanager.com',
-        githubUrl: 'https://github.com/tuusuario/task-manager',
+        demoUrl: 'https://funny-valkyrie-1651a5.netlify.app/',
+        githubUrl: 'https://github.com/Antonio-DelaRua/React_projects',
         image: 'assets/react.png'
       },
 
       {
         title: 'Portfolio Personal',
         description: 'Mi portfolio personal desarrollado con Angular.',
-        technologies: ['Angular', 'TypeScript', 'CSS3', 'RxJS'],
-        demoUrl: 'https://mi-portfolio.com',
-        githubUrl: 'https://github.com/tuusuario/portfolio'
+        technologies: ['Html', 'CSS', 'Javascript', 'RxJS'],
+        demoUrl: 'https://github.com/Antonio-DelaRua/RuXxDeSiNgWEB',
+        githubUrl: 'https://github.com/tuusuario/portfolio',
+        image: 'assets/html.jpg'
       },
       {
-        title: 'Chat App',
-        description: 'Aplicación de mensajería en tiempo real.',
-        technologies: ['Angular', 'Socket.io', 'Node.js', 'MongoDB'],
-        demoUrl: 'https://demo-chat.com',
-        githubUrl: 'https://github.com/tuusuario/chat-app'
+        title: 'Shimeji con llm local',
+        description: 'Aplicación chat ia, con automatizacion por voz, y con llm especializado en python',
+        technologies: ['Python', 'Sqlite', 'Tkinter', 'langchain'],
+        demoUrl: 'https://github.com/Antonio-DelaRua/Selina_IA',
+        githubUrl: 'https://github.com/Antonio-DelaRua',
+        image: 'assets/giphy.gif'
       },{
-        title: 'Portfolio Personal',
+        title: 'Ruta de aprendizaje Spring Boot',
         description: 'Mi portfolio personal desarrollado con Angular.',
-        technologies: ['Angular', 'TypeScript', 'CSS3', 'RxJS'],
+        technologies: ['SpringBoot', 'Maven', 'ApiRest', 'Pdf'],
         demoUrl: 'https://mi-portfolio.com',
-        githubUrl: 'https://github.com/tuusuario/portfolio'
+        githubUrl: 'https://github.com/tuusuario/portfolio',
+        image: 'assets/maven.gif'
       },
       {
-        title: 'Portfolio Personal',
+        title: 'Bootcamp Angular online',
         description: 'Mi portfolio personal desarrollado con Angular.',
-        technologies: ['Angular', 'TypeScript', 'CSS3', 'RxJS'],
+        technologies: ['Angular', 'TypeScript', 'Scss', 'RxJS'],
         demoUrl: 'https://mi-portfolio.com',
-        githubUrl: 'https://github.com/tuusuario/portfolio'
+        githubUrl: 'https://github.com/tuusuario/portfolio',
+        image: 'assets/abt.png'
       },
 
     ];
