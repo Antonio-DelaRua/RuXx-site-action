ec7f310ae6b54e1d82094f499cf6cc51037a9245
Antonio-DelaRua <ruxxdeveloper@gmail.com>
2025-10-13 23:46:56 +0200

    routing-home

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar';
+import { Hero } from '../../components/hero/hero';
+import { Proyectos } from '../../components/proyectos/proyectos';
+import { RutalearningComponent } from '../../components/rutalearning/rutalearning';
+import { Certificates } from '../../components/certificates/certificates';
+import { Contact } from '../../components/contact/contact';
+import { Footer } from '../../components/footer/footer';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,                    
+    RouterModule,                    
+    NavbarComponent,
+    Hero,
+    Proyectos,
+    RutalearningComponent,
+    Certificates,
+    Contact,
+    Footer
+  ],
+  templateUrl: './home.html',
+  styleUrl: './home.css'
+})
+export class Home {
+
+}
