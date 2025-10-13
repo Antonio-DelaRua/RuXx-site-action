f449fab83f1dc07537ac8a241ba75eebfa46b974
Antonio-DelaRua <ruxxdeveloper@gmail.com>
2025-10-13 18:50:02 +0200

    renovacion-porfolio

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,3 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
-import { Skills } from './pages/skills/skills';
-import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [
-  {
-    path: '',
-    component: Home,
-    title: 'RuXxDev - Inicio'
-  },
-    {
-    path: 'skills',
-    component: Skills,
-    title: 'RuXxDev - Skills'
-  },
-      {
-    path: 'Contacto',
-    component: Contact,
-    title: 'RuXxDev - Contacto'
-  },
-  {
-    path: '**',
-    redirectTo: '',
-    pathMatch: 'full'
-  }
-];
+export const routes: Routes = [];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavBar } from "./components/navbar/navbar";
+import { NavbarComponent } from "./components/navbar/navbar";
+import { Hero } from "./components/hero/hero";
+import { Proyectos } from "./components/proyectos/proyectos";
+import { RutalearningComponent } from "./components/rutalearning/rutalearning";
+import { Certificates } from './components/certificates/certificates';
+import { Contact } from './components/contact/contact';
 import { Footer } from "./components/footer/footer";
-
-
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar, Footer],
-  templateUrl: 'app.html',
-  styleUrl: 'app.css'
+  imports: [RouterOutlet, NavbarComponent, Hero, Proyectos, RutalearningComponent, Certificates, Contact, Footer],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('RuXx-site');
+  protected readonly title = signal('Selina-web');
 }
