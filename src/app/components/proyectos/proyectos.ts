36c51ca4cbb59404accf4859136affec2b430f59
Antonio-DelaRua <ruxxdeveloper@gmail.com>
2025-10-14 16:43:11 +0200

    traduccion-nav-hero-proyec

--- src/app/components/proyectos/proyectos.ts
+++ src/app/components/proyectos/proyectos.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LanguageService } from '../../services/language-service';
 
 @Component({
   selector: 'app-proyectos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, TranslateModule],
   templateUrl: './proyectos.html',
   styleUrl: './proyectos.css'
 })
-export class Proyectos {
+export class Proyectos implements OnInit {
 
-}
+  constructor(
+    public translate: TranslateService,
+    private languageService: LanguageService 
+  ) {}
+
+  get PROYECTOS() {
+    return this.translate.instant('PROYECTOS');
+  }
+
+  ngOnInit() {
+    // Suscribirse a los cambios de idioma del servicio
+    this.languageService.currentLang$.subscribe(lang => {
+      this.translate.use(lang);
+    });
+  }
+    }
